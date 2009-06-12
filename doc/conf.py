8a9c3d742b80af0b594106f7021e16eda4372390
Anthony Tuininga <anthony.tuininga@gmail.com>
2009-06-12 18:40:23 +0000

    Updated documentation in preparation for release.

--- doc/conf.py
+++ doc/conf.py
@@ -40,9 +40,9 @@ copyright = '2008, Anthony Tuininga'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.4'
+version = '2.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.4'
+release = '2.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
