3a5e0a9794d11d00cab7201c9207a2d74bd84570
Anthony Tuininga <anthony.tuininga@gmail.com>
2018-06-16 14:23:00 -0600

    Reorganize documentation and remove stored documentation (found on ReadTheDocs anyway).

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -31,15 +31,15 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'cx_Logging'
-copyright = '2010, Anthony Tuininga'
+copyright = '2018, Anthony Tuininga'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.1'
+version = '2.2'
 # The full version, including alpha/beta/rc tags.
-release = '2.1'
+release = '2.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
