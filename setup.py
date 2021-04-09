fb5ef1ddc462166845adee822c3bd70336341d87
Anthony Tuininga <anthony.tuininga@gmail.com>
2021-04-08 21:35:49 -0600

    Convert to PEP 8 compliant style.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 # if setuptools is detected, use it to add support for eggs
 try:
     from setuptools import setup, Extension
-except:
+except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
@@ -22,9 +22,6 @@ BUILD_VERSION = "3.0"
 # define class to ensure that linking against the library works for normal
 # C programs while maintaining the name that Python expects
 class build_ext(distutils.command.build_ext.build_ext):
-    import distutils
-    import os
-    import sys
     if sys.platform == "win32":
         user_options = distutils.command.build_ext.build_ext.user_options + [
                 ('build-implib=', None,
@@ -32,46 +29,38 @@ class build_ext(distutils.command.build_ext.build_ext):
         ]
 
     def build_extension(self, ext):
-        import distutils.command.build_ext
-        import os
-        import sys
-        extraLinkArgs = ext.extra_link_args = []
+        extra_link_args = ext.extra_link_args = []
         if sys.platform == "win32":
             self.mkpath(self.build_implib)
             if self.compiler.compiler_type == "msvc":
-                self.importLibraryName = os.path.join(self.build_implib,
+                self.import_library_name = os.path.join(self.build_implib,
                         "%s.lib" % ext.name)
-                extraLinkArgs.append("/IMPLIB:%s" % self.importLibraryName)
+                extra_link_args.append("/IMPLIB:%s" % self.import_library_name)
             else:
-                self.importLibraryName = os.path.join(self.build_implib,
+                self.import_library_name = os.path.join(self.build_implib,
                         "lib%s.a" % ext.name)
-                extraLinkArgs.append("-Wl,--add-stdcall-alias")
-                extraLinkArgs.append("-Wl,--enable-stdcall-fixup")
-                extraLinkArgs.append("-Wl,--out-implib=%s" % \
-                        self.importLibraryName)
+                extra_link_args.append("-Wl,--add-stdcall-alias")
+                extra_link_args.append("-Wl,--enable-stdcall-fixup")
+                extra_link_args.append("-Wl,--out-implib=%s" % \
+                        self.import_library_name)
             ext.libraries = ["ole32"]
         else:
-            fileName = self.get_ext_filename(ext.name)
+            file_name = self.get_ext_filename(ext.name)
             if sys.platform.startswith("aix"):
-                extraLinkArgs.append("-Wl,-so%s" % fileName)
+                extra_link_args.append("-Wl,-so%s" % file_name)
             else:
-                extraLinkArgs.append("-Wl,-soname,%s" % fileName)
+                extra_link_args.append("-Wl,-soname,%s" % file_name)
         distutils.command.build_ext.build_ext.build_extension(self, ext)
 
     def finalize_options(self):
-        import distutils.command.build_ext
         distutils.command.build_ext.build_ext.finalize_options(self)
-        import os
-        import sys
         if sys.platform == "win32" and self.build_implib is None:
-            dir = "implib.%s-%s" % \
+            target_dir = "implib.%s-%s" % \
                     (distutils.util.get_platform(), sys.version[:3])
-            self.build_implib = os.path.join("build", dir)
+            self.build_implib = os.path.join("build", target_dir)
 
     def initialize_options(self):
-        import distutils.command.build_ext
         distutils.command.build_ext.build_ext.initialize_options(self)
-        import sys
         if sys.platform == "win32":
             self.build_implib = None
 
@@ -84,21 +73,21 @@ class install_data(distutils.command.install_data.install_data):
         distutils.command.install_data.install_data.run(self)
         if sys.platform == "win32":
             command = self.get_finalized_command("build_ext")
-            dir = os.path.join(self.install_dir, "Libs")
-            self.mkpath(dir)
-            baseName = os.path.basename(command.importLibraryName)
-            targetFileName = os.path.join(dir, baseName)
-            self.copy_file(command.importLibraryName, targetFileName)
-            self.outfiles.append(targetFileName)
+            target_dir = os.path.join(self.install_dir, "Libs")
+            self.mkpath(target_dir)
+            base_name = os.path.basename(command.import_library_name)
+            target_file_name = os.path.join(target_dir, base_name)
+            self.copy_file(command.import_library_name, target_file_name)
+            self.outfiles.append(target_file_name)
 
 
 # setup macros
-defineMacros = [
+define_macros = [
         ("CX_LOGGING_CORE",  None),
         ("BUILD_VERSION", BUILD_VERSION)
 ]
 
-exportSymbols = [
+export_symbols = [
         "StartLogging",
         "StartLoggingEx",
         "StartLoggingForPythonThread",
@@ -134,7 +123,7 @@ exportSymbols = [
 ]
 
 if sys.platform == "win32":
-    exportSymbols.extend([
+    export_symbols.extend([
             "LogWin32Error",
             "LogGUID",
             "StartLoggingW",
@@ -150,14 +139,14 @@ if sys.platform == "win32":
 
 # setup the extension
 extension = Extension(
-        name = "cx_Logging",
-        define_macros = defineMacros,
-        export_symbols = exportSymbols,
-        sources = ["src/cx_Logging.c"],
-        depends = ["src/cx_Logging.h"])
+        name="cx_Logging",
+        define_macros=define_macros,
+        export_symbols=export_symbols,
+        sources=["src/cx_Logging.c"],
+        depends=["src/cx_Logging.h"])
 
 # perform the setup
 setup(
-        cmdclass = dict(build_ext = build_ext, install_data = install_data),
-        version = BUILD_VERSION,
-        ext_modules = [extension])
+        cmdclass=dict(build_ext=build_ext, install_data=install_data),
+        version=BUILD_VERSION,
+        ext_modules=[extension])
