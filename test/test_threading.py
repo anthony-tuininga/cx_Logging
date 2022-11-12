53baac676f27cede772eae99e0bf5bd37300dc36
Anthony Tuininga <anthony.tuininga@gmail.com>
2022-11-12 15:23:16 -0700

    Use PEP 8 formatting.

--- test/test_threading.py
+++ test/test_threading.py
@@ -4,31 +4,30 @@ import sys
 import threading
 
 if len(sys.argv) > 1:
-    numThreads = int(sys.argv[1])
+    num_threads = int(sys.argv[1])
 else:
-    numThreads = 5
+    num_threads = 5
 if len(sys.argv) > 2:
-    numIterations = int(sys.argv[2])
+    num_iters = int(sys.argv[2])
 else:
-    numIterations = 1000
+    num_iters = 1000
 
-def Run(threadNum):
-    cx_Logging.Debug("Thread-%d: starting", threadNum)
-    iterationsLeft = numIterations
-    while iterationsLeft > 0:
-        numFiles = len(os.listdir("."))
+def run(thread_num):
+    cx_Logging.Debug("Thread-%d: starting", thread_num)
+    iters_left = num_iters
+    while iters_left > 0:
+        num_files = len(os.listdir("."))
         cx_Logging.Debug("Thread-%d: counted %d files, %d iterations left",
-                threadNum, numFiles, iterationsLeft)
-        iterationsLeft -= 1
+                thread_num, num_files, iters_left)
+        iters_left -= 1
 
-cx_Logging.StartLogging("test_threading.log", level = cx_Logging.DEBUG,
-        maxFiles = 10, maxFileSize = 5 * 1024 * 1024, prefix = "[%i] %t")
-cx_Logging.Debug("Testing logging with %s threads.", numThreads)
+cx_Logging.StartLogging("test_threading.log", level=cx_Logging.DEBUG,
+        maxFiles=10, maxFileSize=5 * 1024 * 1024, prefix="[%i] %t")
+cx_Logging.Debug("Testing logging with %s threads.", num_threads)
 threads = []
-for i in range(numThreads):
-    thread = threading.Thread(target = Run, args = (i + 1,))
+for i in range(num_threads):
+    thread = threading.Thread(target=run, args=(i + 1,))
     threads.append(thread)
     thread.start()
 for thread in threads:
     thread.join()
-
